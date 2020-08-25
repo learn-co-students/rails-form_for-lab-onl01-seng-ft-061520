df30a77012e5d0d584bf22f9d8a2d5a91b28a7a1
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2020-08-25 15:20:48 -0600

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,23 +5,27 @@ class StudentsController < ApplicationController
     end
     
     def new
-        @studnet = Student.new
+        @student = Student.new
+    end
+
+    def edit
+        @student = Student.find(params[:id])
     end
 
     def create 
-        @student = Student.new(student_params(:first_name, :last_name))
+        @student = Student.new(student_params)
         @student.save
         redirect_to student_path(@student)
     end
 
     def update 
         @student = Student.find(params[:id])
-        @student.update(student_params(:first_name, :last_name))
+        @student.update(student_params)
         redirect_to student_path(@student)
     end
-    
+
     private 
-    def student_params(*args)
-        params.require(:student).permit(*args)
+    def student_params
+        params.require(:student).permit(:first_name, :last_name)
     end
 end

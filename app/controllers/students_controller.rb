783d3c2ee729525a2c5e7f52bdb9149cbdb3d06c
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2020-08-25 14:54:06 -0600

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,26 @@
 class StudentsController < ApplicationController
+    
+    def show
+        @student = Student.find(params[:id])
+    end
+    
+    def new
+        @studnet = Student.new
+    end
 
+    def create 
+        @student = Student.new(student_params)
+        @student.save
+        redirect_to student_path(@student)
+    end
+
+    def update 
+        @student = Student.find(params[:id])
+        @student.update(student_params(:first_name, :last_name))
+        redirect_to student_path(@student)
+    end
+    private 
+    def student_params(*args)
+        params.require(:student).permit(*args)
+    end
 end
--- app/models/school_class.rb
+++ app/models/school_class.rb
@@ -1,2 +1,3 @@
 class SchoolClass < ApplicationRecord
+    #title:string, room_number:integer
 end

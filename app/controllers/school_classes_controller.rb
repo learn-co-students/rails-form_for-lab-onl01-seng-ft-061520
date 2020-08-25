34ad13b1d29dab0e4a8fd2808695efa4b897b536
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2020-08-25 12:58:35 -0600

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,8 +1,13 @@
 class SchoolClassesController < ApplicationController
     def new 
+        @school_class = SchoolClass.find(params[:id])
         @school_class = SchoolClass.new
     end
 
+    def show
+		@school_class = SchoolClass.find(params[:id])
+    end
+    
     def create 
         @school_class = SchoolClass.new(school_class_params(:title, :room_number))
         @school_class.save
